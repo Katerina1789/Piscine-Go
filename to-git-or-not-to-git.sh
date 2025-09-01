812bdc4e65736b17b45eaecf5fef2c97687d8013
kkasdana <kadkat1789@gmail.com>
2025-09-01 15:43:08 +0300

    Add to-git-or-not-to-git script

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-curl https://platform.zone01.gr/assets/superhero/all.json
-jq '.[] | select(.id == 170) | "\(.name) - \(.power) - \(.gender)"'
+curl https://platform.zone01.gr/assets/superhero/all.json | jq '.[] | select(.id == 170) | .name, .powerstats.power, .appearance.gender'
