9114c3f4d62569bf6657842705120ba984a207f4
Katerina Kasdanastasi <kkasdana@noreply.platform.zone01.gr>
2025-09-03 11:40:48 +0000

    Update printalphabet/main.go

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -4,7 +4,7 @@ import "github.com/01-edu/z01"
 
 func main() {
 	ab := "abcdefghijklmnopqrstuvwxyz"
-	for i := 0; i < 25; i++ {
+	for i := 0; i < 26; i++ {
 		z01.PrintRune(rune(ab[i]))
 	}
 	z01.PrintRune('\n')
