b4190df60de0b9cf50c4faca0e6504f40adffb9a
kkasdana <kadkat1789@gmail.com>
2025-09-08 16:33:09 +0300

    Add go file

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -7,9 +7,9 @@ func RecursiveFactorial(nb int) int {
 	if nb == 0 {
 		return 1
 	}
-	result := 1
-	if nb > 0 && nb < 20 {
-		result = nb * RecursiveFactorial(nb-1)
+	result := nb * RecursiveFactorial(nb-1)
+	if nb < 21 {
+		return 0
 	}
 	if result < 0 {
 		return 0
