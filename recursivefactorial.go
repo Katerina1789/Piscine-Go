4c75dffbb30aa360761afc4ee64bfe13340a4e87
Katerina Kasdanastasi <kkasdana@noreply.platform.zone01.gr>
2025-09-08 13:12:37 +0000

    Update recursivefactorial.go

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -7,7 +7,7 @@ func RecursiveFactorial(nb int) int {
 	if nb == 0 {
 		return 1
 	}
-	result := nb * RecursiveFactorial(nb-1)
+	result := nb * RecursiveFactorial(nb - 1)
 	if result < 0 {
 		return 0
 	}
