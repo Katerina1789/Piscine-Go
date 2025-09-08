81d63e48e5553ecf7ee02157bef7b6efbd897225
kkasdana <kadkat1789@gmail.com>
2025-09-08 16:39:47 +0300

    Add go file

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,16 +1,13 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb < 0 {
+	if nb < 0 && nb > 21 {
 		return 0
 	}
-	if nb == 0 || nb == 1 {
+	if nb == 0 {
 		return 1
 	}
 	result := nb * RecursiveFactorial(nb-1)
-	if nb > 1 && nb < 21 {
-		return result
-	}
 	if result < 0 {
 		return 0
 	}
