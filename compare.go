9ede1a7c9fa91e3e3a2f17abc946fd87fa0c4ff2
kkasdana <kadkat1789@gmail.com>
2025-09-10 02:03:41 +0300

    Update

--- compare.go
+++ compare.go
@@ -5,11 +5,16 @@ func Compare(a, b string) int {
 	if len(b) < minLen {
 		minLen = len(b)
 	}
-	for i := 0; i < minLen; i++ {
+	for i := 0; i <= minLen; i++ {
 		if a[i] < b[i] {
 			return -1
 		} else if a[i] > b[i] {
 			return 1
+		}
+		if len(a) > len(b) {
+			return 1
+		} else if len(a) < len(b) {
+			return -1
 		}
 	}
 	return 0
