b6f8ae9fc69b373dd96447f376a68ceb00c68059
kkasdana <kadkat1789@gmail.com>
2025-09-10 01:58:55 +0300

    Add go file

---
+++ compare.go
@@ -0,0 +1,16 @@
+package piscine
+
+func Compare(a, b string) int {
+	minLen := len(a)
+	if len(b) < minLen {
+		minLen = len(b)
+	}
+	for i := 0; i <= minLen; i++ {
+		if a[i] < b[i] {
+			return -1
+		} else if a[i] > b[i] {
+			return 1
+		}
+	}
+	return 0
+}
