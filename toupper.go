39a8125bbd4c49e021bc80ee04973eb857f26e77
kkasdana <kadkat1789@gmail.com>
2025-09-10 03:39:17 +0300

    Update

--- toupper.go
+++ toupper.go
@@ -3,7 +3,7 @@ package piscine
 func ToUpper(s string) string {
 	result := ""
 	for _, r := range s {
-		if r >= 'a' || r <= 'z' {
+		if r >= 'a' && r <= 'z' {
 			result += string(r - 32)
 		} else {
 			result += string(r)
