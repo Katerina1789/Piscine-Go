45d788700738febdc7372829ef68669f034b7dd2
kkasdana <kadkat1789@gmail.com>
2025-09-10 23:35:01 +0300

    Update

--- trimatoi.go
+++ trimatoi.go
@@ -7,11 +7,11 @@ func TrimAtoi(s string) int {
 	for i := 0; i < len(s); i++ {
 		if s[i] == '-' {
 			sign = -1
+			return sign
 		}
 		if s[i] >= '0' && s[i] <= '9' {
 			number = number*10 + int(s[i]-'0')
 		}
 	}
-
-	return number * sign
+	return number
 }
