3d30ccebe9772af80969b7ef572ba26221ae37ed
kkasdana <kadkat1789@gmail.com>
2025-09-10 03:12:02 +0300

    Update

--- isalpha.go
+++ isalpha.go
@@ -2,11 +2,7 @@ package piscine
 
 func IsAlpha(s string) bool {
 	for _, r := range s {
-		if r < '0' || r > '9' {
-			return false
-		} else if r < 'A' || r > 'Z' {
-			return false
-		} else if r < 'a' || r > 'z' {
+		if (r < '0' || r > '9') && (r < 'A' || r > 'Z') && (r < 'a' || r > 'z') {
 			return false
 		}
 	}
