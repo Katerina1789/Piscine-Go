40f6ef4934f01919ceccb36a4f99f680a8c9885a
kkasdana <kadkat1789@gmail.com>
2025-09-10 23:39:50 +0300

    Update

--- trimatoi.go
+++ trimatoi.go
@@ -3,12 +3,14 @@ package piscine
 func TrimAtoi(s string) int {
 	sign := 1
 	number := 0
+	foundDigit := true
 
 	for i := 0; i < len(s); i++ {
-		if s[i] == '-' {
+		if s[i] == '-' && foundDigit {
 			sign = -1
 		}
 		if s[i] >= '0' && s[i] <= '9' {
+			foundDigit = false
 			number = number*10 + int(s[i]-'0')
 		}
 	}
