603af24e72f37198739029cc58e9e4de6cfdb7e3
kkasdana <kadkat1789@gmail.com>
2025-09-11 00:34:37 +0300

    Update

--- join.go
+++ join.go
@@ -3,7 +3,10 @@ package piscine
 func Join(strs []string, sep string) string {
 	result := ""
 	for i := 0; i < len(strs); i++ {
-		result += (strs[i] + sep)
+		result += strs[i]
+		if i < len(strs)-1 {
+			result += sep
+		}
 	}
 	return result
 }
