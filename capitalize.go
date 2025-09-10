1f289b05701bb7527ae515e361a9d1e92d5f37a7
kkasdana <kadkat1789@gmail.com>
2025-09-11 00:03:30 +0300

    Add go file

---
+++ capitalize.go
@@ -0,0 +1,25 @@
+package piscine
+
+func Capitalize(s string) string {
+	runes := []rune(s)
+	newWord := true
+
+	for i := 0; i < len(runes); i++ {
+		if (runes[i] >= 'a' && runes[i] <= 'z') || (runes[i] >= 'A' && runes[i] <= 'Z') || (runes[i] >= '0' && runes[i] <= '9') {
+			if newWord {
+				if runes[i] >= 'a' && runes[i] <= 'z' {
+					runes[i] = -32
+				} else if !newWord {
+					if runes[i] >= 'A' && runes[i] <= 'Z' {
+						runes[i] += 32
+					}
+				}
+			} else {
+				newWord = true
+			}
+		}
+	}
+
+	return string(runes)
+
+}
