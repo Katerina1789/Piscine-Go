8c0c98643d9b76b1d20903a35562659d14464242
kkasdana <kadkat1789@gmail.com>
2025-09-10 13:03:24 +0300

    Add go file

---
+++ printnbrinorder.go
@@ -0,0 +1,18 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbrInOrder(n int) {
+	count := [10]int{}
+	for n >= 0 {
+		digit := n % 10
+		count[digit]++
+		n /= 10
+	}
+	for i := 0; i < 10; i++ {
+		for count[i] >= 0 {
+			z01.PrintRune(rune(i))
+			count[i]--
+		}
+	}
+}
