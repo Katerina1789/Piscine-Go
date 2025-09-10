6018299728ae9457f913a719f61f9e542545e58a
kkasdana <kadkat1789@gmail.com>
2025-09-10 23:33:22 +0300

    Add go file

---
+++ trimatoi.go
@@ -0,0 +1,17 @@
+package piscine
+
+func TrimAtoi(s string) int {
+	sign := 1
+	number := 0
+
+	for i := 0; i < len(s); i++ {
+		if s[i] == '-' {
+			sign = -1
+		}
+		if s[i] >= '0' && s[i] <= '9' {
+			number = number*10 + int(s[i]-'0')
+		}
+	}
+
+	return number * sign
+}
