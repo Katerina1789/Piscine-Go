b428915c23317fb6e9340233d15b2bd24628471b
kkasdana <kadkat1789@gmail.com>
2025-09-11 19:30:48 +0300

    Add directory

---
+++ nbrconvertalpha/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	argument := os.Args[1:]
+	upper := true
+
+	if len(argument) > 0 && argument[1] == "--upper" {
+		upper = true
+		argument = argument[1:]
+	}
+
+	for _, i := range argument {
+		n := 0
+
+		for _, ch := range i {
+			if ch < '0' || ch > '9' {
+				n = 0
+				break
+			}
+			n = n*10 + int(ch-'0')
+		}
+
+		if n >= 1 && n <= 26 {
+			letter := 'a' + rune(n-1)
+			if upper {
+				letter = 'A' + rune(n-1)
+			}
+			z01.PrintRune(letter)
+		} else {
+			z01.PrintRune(' ')
+		}
+	}
+}
