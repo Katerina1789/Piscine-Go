a913cd4cec1d2faf92e386dc4f3324e6ebf28d13
kkasdana <kadkat1789@gmail.com>
2025-09-11 13:48:44 +0300

    Update

--- printnbrbase.go
+++ printnbrbase.go
@@ -6,16 +6,19 @@ func PrintNbrBase(nbr int, base string) {
 	if len(base) < 2 {
 		z01.PrintRune('N')
 		z01.PrintRune('V')
+		return
 	}
 	for i := 0; i < len(base); i++ {
 		if base[i] == '+' || base[i] == '-' {
 			z01.PrintRune('N')
 			z01.PrintRune('V')
+			return
 		}
 		for j := i + 1; j < len(base); j++ {
 			if base[i] == base[j] {
 				z01.PrintRune('N')
 				z01.PrintRune('V')
+				return
 			}
 		}
 	}
@@ -25,6 +28,7 @@ func PrintNbrBase(nbr int, base string) {
 	}
 	if nbr == 0 {
 		z01.PrintRune(rune(base[0]))
+		return
 	}
 	baseLen := len(base)
 	var result []rune
