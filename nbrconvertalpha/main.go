cdf540c70c9f5b662cf804384e62a98fc0e9aac2
kkasdana <kadkat1789@gmail.com>
2025-09-11 19:40:38 +0300

    Update directory

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -25,7 +25,6 @@ func main() {
 			}
 			n = n*10 + int(ch-'0')
 		}
-		z01.PrintRune('\n')
 
 		if n >= 1 && n <= 26 {
 			letter := 'a' + rune(n-1)
@@ -37,4 +36,5 @@ func main() {
 			z01.PrintRune(' ')
 		}
 	}
+	z01.PrintRune('\n')
 }
