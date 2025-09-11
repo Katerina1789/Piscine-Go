685e28a5428a6a14183008e7b92401f265aeb4a2
kkasdana <kadkat1789@gmail.com>
2025-09-11 16:26:00 +0300

    Update directory

--- sortparams/main.go
+++ sortparams/main.go
@@ -8,15 +8,15 @@ import (
 
 func main() {
 	argument := os.Args
-	for i := 1; i < len(argument); i++ {
+	for i := 1; i < len(argument)-1; i++ {
 		for j := i + 1; j < len(argument); j++ {
 			if argument[i] > argument[j] {
 				argument[i], argument[j] = argument[j], argument[i]
 			}
 		}
 	}
-	for _, word := range argument {
-		for _, ch := range word {
+	for i := 1; i < len(argument); i++ {
+		for _, ch := range argument[i] {
 			z01.PrintRune(ch)
 		}
 		z01.PrintRune('\n')
