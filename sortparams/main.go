9eb1adc99ee8f82cb8422120858095c18d02b259
kkasdana <kadkat1789@gmail.com>
2025-09-11 16:23:26 +0300

    Update directory

--- sortparams/main.go
+++ sortparams/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	argument := os.Args
 	for i := 1; i < len(argument); i++ {
-		for j := i + 1; j < len(argument)-1; j++ {
+		for j := i + 1; j < len(argument); j++ {
 			if argument[i] > argument[j] {
 				argument[i], argument[j] = argument[j], argument[i]
 			}
