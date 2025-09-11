c70c05c7b10a6510628759567f95e50be0132710
kkasdana <kadkat1789@gmail.com>
2025-09-11 15:36:22 +0300

    Add directory

---
+++ sortparams/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	argument := os.Args
+	for i := 1; i < len(argument); i++ {
+		for j := i + 1; j < len(argument); j++ {
+			if argument[i] > argument[j] {
+				argument[i], argument[j] = argument[j], argument[i]
+			}
+		}
+	}
+	for _, word := range argument {
+		for _, ch := range word {
+			z01.PrintRune(ch)
+		}
+		z01.PrintRune('\n')
+	}
+}
