4c23e3ec80b6634c24e27b3bd2e4cb77c90766bf
kkasdana <kadkat1789@gmail.com>
2025-09-11 15:10:11 +0300

    Update directory

--- printparams/main.go
+++ printparams/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	argument := os.Args
-	for i := 0; i < len(argument); i++ {
+	for i := 1; i < len(argument); i++ {
 		for _, ch := range argument[i] {
 			z01.PrintRune(ch)
 		}
