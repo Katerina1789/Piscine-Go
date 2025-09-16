380cb5dcfba7fec9a20562216e2616f1718e1e7c
kkasdana <kadkat1789@gmail.com>
2025-09-16 23:32:53 +0300

    Update go file

--- issorted.go
+++ issorted.go
@@ -1,10 +1,14 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
-	for i := 0; i < len(a)-1; i++ {
+	for i := 0; i < len(a); i++ {
 		if f(a[i], a[i+1]) > 0 {
 			return false
 		}
+		if f(a[i], a[i+1]) < 0 {
+			return false
+		}
 	}
+
 	return true
 }
