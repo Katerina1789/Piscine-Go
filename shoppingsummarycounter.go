a836a29f628f7953f2f985393c631b828a00be3b
kkasdana <kadkat@gmail.com>
2025-09-18 17:05:33 +0300

    Add go

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -3,26 +3,18 @@ package piscine
 func ShoppingSummaryCounter(str string) map[string]int {
 	result := make(map[string]int)
 	word := ""
-	spaceCount := 0
 
 	for i := 0; i < len(str); i++ {
 		c := str[i]
 
 		if c == ' ' {
-			// Αν υπάρχει ήδη λέξη, την αποθηκεύουμε και ξεκινάμε νέα
 			if word != "" {
 				result[word]++
 				word = ""
-			} else {
-				// Διαδοχικά ή αρχικά κενά -> μετράμε ως extra spaces
-				spaceCount++
 			}
+			// κάθε space προσθέτει 1 στο ""
+			result[""]++
 		} else {
-			// Αν υπήρχαν spaces πριν από γράμμα, τα καταχωρούμε πρώτα
-			if spaceCount > 0 {
-				result[""] += spaceCount
-				spaceCount = 0
-			}
 			word += string(c)
 		}
 	}
@@ -30,11 +22,6 @@ func ShoppingSummaryCounter(str string) map[string]int {
 	// Αν τελειώνει με λέξη
 	if word != "" {
 		result[word]++
-	}
-
-	// Αν τελειώνει με spaces
-	if spaceCount > 0 {
-		result[""] += spaceCount
 	}
 
 	return result
