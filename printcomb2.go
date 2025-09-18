abc2422a72b93c8b9224c12a7a6e47e026475b8f
kkasdana <kadkat@gmail.com>
2025-09-18 15:34:52 +0300

    Add go file

--- printcomb2.go
+++ printcomb2.go
@@ -3,16 +3,19 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	for a := '0'; a <= 98; a++ {
-		for b := a + '1'; b <= 99; b++ {
-			z01.PrintRune(a)
-			z01.PrintRune(b)
-			if a == '9' && b == '9' {
-				z01.PrintRune('\n')
-			} else {
+	for a := 0; a <= 98; a++ {
+		for b := a + 1; b <= 99; b++ {
+			z01.PrintRune(rune(a/10 + '0'))
+			z01.PrintRune(rune(a%10 + '0'))
+			z01.PrintRune(' ')
+			z01.PrintRune(rune(b/10 + '0'))
+			z01.PrintRune(rune(b%10 + '0'))
+
+			if !(a == 98 && b == 99) {
 				z01.PrintRune(',')
 				z01.PrintRune(' ')
 			}
 		}
 	}
+	z01.PrintRune('\n')
 }
