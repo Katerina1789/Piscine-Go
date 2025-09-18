862761f3f2851221881a92c2369208b91c50c84c
kkasdana <kadkat@gmail.com>
2025-09-19 01:53:39 +0300

    Add file

---
+++ comcheck/main.go
@@ -0,0 +1,16 @@
+package piscine
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	for i := 0; i < len(args); i++ {
+		if args[i] == "01" || args[i] == "galaxy" || args[i] == "galaxy 01" {
+			fmt.Println("Alert!!!")
+			return
+		}
+	}
+}
