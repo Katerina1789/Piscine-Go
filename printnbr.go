2cc2a58c86863fb3034f5cdcc4af77f67c59cf09
kkasdana <kadkat@gmail.com>
2025-09-18 18:29:07 +0300

    Add go

--- printnbr.go
+++ printnbr.go
@@ -1,20 +1,4 @@
 package piscine
 
-import "github.com/01-edu/z01"
-
 func PrintNbr(n int) {
-	if n == -2147483648 {
-		z01.PrintRune('-')
-		z01.PrintRune('2')
-		PrintNbr(147483648)
-		return
-	}
-	if n < 0 {
-		z01.PrintRune('-')
-		n = -n
-	}
-	if n >= 10 {
-		PrintNbr(n / 10)
-	}
-	z01.PrintRune(rune(n%10 + '0'))
 }
