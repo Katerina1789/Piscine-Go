81a8bc803ae2bce4f40b58e7df78853a4c1a4f90
kkasdana <kadkat@gmail.com>
2025-09-18 15:20:08 +0300

    Add go file

--- abort.go
+++ abort.go
@@ -4,8 +4,8 @@ func Abort(a, b, c, d, e int) int {
 	nums := [5]int{a, b, c, d, e}
 	for i := 0; i < 4; i++ {
 		for j := 0; j < 4-i; j++ {
-			if nums[j] > nums[j-i] {
-				nums[j], nums[j+i] = nums[j+i], nums[j]
+			if nums[j] > nums[j+1] {
+				nums[j], nums[j+1] = nums[j+1], nums[j]
 			}
 		}
 	}
