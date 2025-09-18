86c3acdd9096268897d1c39106a2f07f2838cb61
kkasdana <kadkat@gmail.com>
2025-09-18 17:08:06 +0300

    Add go

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -2,26 +2,49 @@ package piscine
 
 func ShoppingSummaryCounter(str string) map[string]int {
 	result := make(map[string]int)
+
 	word := ""
+	spaceRun := 0    // length of current consecutive spaces
+	sawWord := false // have we completed at least one word yet?
 
 	for i := 0; i < len(str); i++ {
 		c := str[i]
 
 		if c == ' ' {
+			// finalize a word if we were building one
 			if word != "" {
 				result[word]++
 				word = ""
+				sawWord = true
 			}
-			// κάθε space προσθέτει 1 στο ""
-			result[""]++
+			spaceRun++
 		} else {
+			// we’re entering a word after some spaces: account for spaces first
+			if spaceRun > 0 {
+				if sawWord {
+					// between words: only count the extras beyond the first separator
+					if spaceRun > 1 {
+						result[""] += spaceRun - 1
+					}
+				} else {
+					// leading spaces: count them all
+					result[""] += spaceRun
+				}
+				spaceRun = 0
+			}
+			// build the current word
 			word += string(c)
 		}
 	}
 
-	// Αν τελειώνει με λέξη
+	// finalize last word
 	if word != "" {
 		result[word]++
+	}
+
+	// trailing spaces: count them all
+	if spaceRun > 0 {
+		result[""] += spaceRun
 	}
 
 	return result
