d9e42ffcd00e946c0f6f3504fc3e500b3e08313c
kkasdana <kadkat@gmail.com>
2025-09-18 16:54:01 +0300

    Add go

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -29,7 +29,7 @@ func ShoppingSummaryCounter(str string) map[string]int {
 		result[word]++
 	}
 	if extraSpaces > 0 {
-		result[""] = extraSpaces + 1
+		result[""] = extraSpaces
 	}
 	return result
 }
