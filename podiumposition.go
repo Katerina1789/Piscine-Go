d41cad93c7a8631cfba6b97cc869f29987028028
kkasdana <kadkat@gmail.com>
2025-09-19 03:21:34 +0300

    Add file

--- podiumposition.go
+++ podiumposition.go
@@ -1,12 +1,10 @@
 package piscine
 
 func PodiumPosition(podium [][]string) [][]string {
-	var result [][]string
 	i := 0
-	for i < len(podium) {
-		result = result[:len(result)+1] // manually extend the slice
-		result[len(result)-1] = podium[len(podium)-1-i]
+	for i < len(podium)/2 {
+		podium[i], podium[len(podium)-1-i] = podium[len(podium)-1-i], podium[i]
 		i++
 	}
-	return result
+	return podium
 }
