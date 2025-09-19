418d6f4e52b0fcbc0ae4340d068e55722d07ade4
kkasdana <kadkat@gmail.com>
2025-09-19 03:14:42 +0300

    Add file

--- reversemenuindex.go
+++ reversemenuindex.go
@@ -1,9 +1,9 @@
 package piscine
 
 func ReverseMenuIndex(menu []string) []string {
-	order := make([]string, len(menu))
-	for i := 0; i < len(menu); i++ {
-		order[i] = menu[len(menu)-1-i]
+	order := make([]string, len(menu)) // adds to var order the slices of the menu and the length
+	for i := 0; i < len(menu); i++ {   // forloop where i gets added one until it's smaller than the length of the menu
+		order[i] = menu[len(menu)-1-i] //
 	}
 	return order
 }
