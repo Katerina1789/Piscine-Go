ff075f71e2e92d97a231d5c96f4bcee1c84893df
kkasdana <kadkat@gmail.com>
2025-09-19 03:19:32 +0300

    Add file

--- podiumposition.go
+++ podiumposition.go
@@ -4,7 +4,8 @@ func PodiumPosition(podium [][]string) [][]string {
 	var result [][]string
 	i := 0
 	for i < len(podium) {
-		result = append(result, podium[len(podium)-1-i])
+		result = result[:len(result)+1] // manually extend the slice
+		result[len(result)-1] = podium[len(podium)-1-i]
 		i++
 	}
 	return result
