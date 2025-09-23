1407420092d4f352a113addb39cd0a45d575c73a
kkasdana <kadkat1789@gmail.com>
2025-09-23 19:02:23 +0300

    Add go file

--- listpushback.go
+++ listpushback.go
@@ -1,27 +1,29 @@
 package piscine // Defines the package name
 
 // NodeL represents a single element in the linked list
-type NodeL struct {
+type Nodel struct {
 	Data interface{} // Holds any type of data (string, int, etc.)
 	Next *NodeL      // Pointer to the next node in the list (nil if it's the last)
 }
 
 // List represents the linked list itself, with pointers to the first and last nodes
-type List struct {
+type Lis struct {
 	Head *NodeL // Points to the first node in the list
 	Tail *NodeL // Points to the last node in the list
 }
 
 // ListPushBack adds a new node with the given data to the end of the list
 func ListPushBack(l *List, data interface{}) {
-	newNode := &NodeL{Data: data} // Create the new node
+	// Create a new node with the provided data
+	newNode := &NodeL{Data: data}
 
+	// If the list is empty (no Head), set both Head and Tail to the new node
 	if l.Head == nil {
-		l.Head = newNode // First node in the list
-		l.Tail = newNode
-		return
+		l.Head = newNode // First node becomes the Head
+		l.Tail = newNode // And also the Tail since it's the only node
+	} else {
+		// If the list is not empty, link the current Tail to the new node
+		l.Tail.Next = newNode // Set the current Tail's Next to the new node
+		l.Tail = newNode      // Update Tail to point to the new last node
 	}
-
-	l.Tail.Next = newNode // Link new node after Tail
-	l.Tail = newNode      // Update Tail to the new node
 }
---
+++ listpushfront.go
@@ -0,0 +1,26 @@
+package piscine
+
+// NodeL represents a single node in the linked list
+type NodeL struct {
+	Data interface{} // Holds the value (can be any type)
+	Next *NodeL      // Points to the next node in the list
+}
+
+// List represents the linked list with pointers to the first and last nodes
+type List struct {
+	Head *NodeL // First node in the list
+	Tail *NodeL // Last node in the list
+}
+
+// ListPushFront inserts a new node at the beginning of the list
+func ListPushFront(l *List, data interface{}) {
+	newNode := &NodeL{Data: data} // Create a new node with the given data
+
+	newNode.Next = l.Head // Link the new node to the current head
+	l.Head = newNode      // Update the head to be the new node
+
+	if l.Tail == nil {
+		// If the list was empty, set Tail to the new node too
+		l.Tail = newNode
+	}
+}
