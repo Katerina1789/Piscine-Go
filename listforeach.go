c40f735a0995f8b79de16317fc00bc7c95003194
kkasdana <kadkat1789@gmail.com>
2025-09-23 20:53:00 +0300

    Add go file

--- listforeach.go
+++ listforeach.go
@@ -1,27 +1,27 @@
 package piscine
 
-type NodeL struct {
+type NodeL8 struct {
 	Data interface{} // Holds the value
 	Next *NodeL      // Points to the next node
 }
 
-type List struct {
-	Head *NodeL // First node in the list
-	Tail *NodeL // Last node in the list
+type List8 struct {
+	Head *NodeL8 // First node in the list
+	Tail *NodeL8 // Last node in the list
 }
 
 // ListForEach applies the given function f to each node in the list
 func ListForEach(l *List, f func(*NodeL)) {
-	current := l.Head // Start from the head
+	curren := l.Head // Start from the head
 
-	for current != nil {
-		f(current)             // Apply the function to the current node
-		current = current.Next // Move to the next node
+	for curren != nil {
+		f(curren)            // Apply the function to the current node
+		curren = curren.Next // Move to the next node
 	}
 }
 
 // Add2_node adds 2 to int data or appends "2" to string data
-func Add2_node(node *NodeL) {
+func Add2_node(node *NodeL8) {
 	switch v := node.Data.(type) {
 	case int:
 		node.Data = v + 2
@@ -31,7 +31,7 @@ func Add2_node(node *NodeL) {
 }
 
 // Subtract3_node subtracts 3 from int data or appends "-3" to string data
-func Subtract3_node(node *NodeL) {
+func Subtract3_node(node *NodeL8) {
 	switch v := node.Data.(type) {
 	case int:
 		node.Data = v - 3
---
+++ listforeachif.go
@@ -0,0 +1,52 @@
+package piscine
+
+type NodeL struct {
+	Data interface{} // Holds any type of value (int, string, etc.)
+	Next *NodeL      // Points to the next node in the list
+}
+
+type List struct {
+	Head *NodeL // Points to the first node in the list
+	Tail *NodeL // Points to the last node in the list
+}
+
+// IsPositiveNode returns true if the node's Data is a positive number
+func IsPositiveNode(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		// Type assertion: convert Data to int and check if it's > 0
+		return node.Data.(int) > 0
+	default:
+		// If Data is not a number, return false
+		return false
+	}
+}
+
+// IsAlNode returns true if the node's Data is NOT a number (e.g., a string)
+func IsAlNode(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		// If Data is a number, return false
+		return false
+	default:
+		// If Data is not a number, return true
+		return true
+	}
+}
+
+// ListForEachIf applies function f to each node in the list l,
+// but only if the condition function cond returns true for that node.
+func ListForEachIf(l *List, f func(*NodeL), cond func(*NodeL) bool) {
+	current := l.Head // Start from the head of the list
+
+	// Traverse the list node by node
+	for current != nil {
+		// Check if the current node satisfies the condition
+		if cond(current) {
+			// If it does, apply the function f to this node
+			f(current)
+		}
+		// Move to the next node in the list
+		current = current.Next
+	}
+}
