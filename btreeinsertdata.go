1b2bc41fd738d5e1a6921e7bc036111f00ba2c52
kkasdana <kadkat1789@gmail.com>
2025-09-24 23:39:12 +0300

    Add go file

--- btreeinsertdata.go
+++ btreeinsertdata.go
@@ -1,24 +1,36 @@
 package piscine
 
-func TreeInsert(root *TreeNode, data int) *TreeNode {
+type TreeNode struct {
+	Left, Right, Parent *TreeNode
+	Data                string
+}
+
+// BTreeInsertData inserts a new node into the binary search tree
+func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+	// If the tree is empty, create and return the new root
 	if root == nil {
-		// If the tree is empty, create a new node
 		return &TreeNode{Data: data}
 	}
 
+	// If the new data is less than current node, go left
 	if data < root.Data {
-		// Insert into the left subtree
-		root.Left = TreeInsert(root.Left, data)
+		if root.Left == nil {
+			// Insert here
+			root.Left = &TreeNode{Data: data, Parent: root}
+		} else {
+			// Recurse into left subtree
+			BTreeInsertData(root.Left, data)
+		}
 	} else {
-		// Insert into the right subtree
-		root.Right = TreeInsert(root.Right, data)
+		// If the new data is greater or equal, go right
+		if root.Right == nil {
+			// Insert here
+			root.Right = &TreeNode{Data: data, Parent: root}
+		} else {
+			// Recurse into right subtree
+			BTreeInsertData(root.Right, data)
+		}
 	}
 
 	return root // Return the unchanged root
-}
-
-type TreeNode struct {
-	Data  int
-	Left  *TreeNode
-	Right *TreeNode
 }
